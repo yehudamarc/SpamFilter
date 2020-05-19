f0936cd002682bb48671a223e10b0cd3ab063931
yehudamarc <yehudamarc@gmail.com>
2020-05-19 12:55:32 +0300

    added proccesing - no training

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import asyncore
 trainData = [] # @TODO: get database for training
 SPAM = True
 #runs once on training data
-def train:
+def train():
     total = 0
     numSpam = 0
     for email in trainData:
@@ -17,8 +17,8 @@ def train:
             numSpam += 1
         total += 1
         processEmail(email.body, email.label)
-    pA = numSpam/(float)total
-    pNotA = (total — numSpam)/(float)total
+    pA = numSpam/total
+    pNotA = (total - numSpam)/total
 
 #counts the words in a specific email
 def processEmail(body, label):
@@ -42,23 +42,40 @@ def conditionalEmail(body, spam):
 #gives the conditional probability p(B_i | A_x)
 def conditionalWord(word, spam):
     if spam:
-       return trainPositive[word]/(float)positiveTotal
-    return trainNegative[word]/(float)negativeTotal
+       return trainPositive[word]/positiveTotal
+    return trainNegative[word]/negativeTotal
 
 #classifies a new email as spam or not spam
 def classify(email):
-    isSpam = pA * conditionalEmail(email, True) # P (A | B)
-    notSpam = pNotA * conditionalEmail(email, False) # P(¬A | B)
-    return isSpam > notSpam
+	return False
+    #isSpam = pA * conditionalEmail(email, True) # P (A | B)
+    #notSpam = pNotA * conditionalEmail(email, False) # P(¬A | B)
+    #return isSpam > notSpam
 
 
 class CustomSMTPServer(smtpd.SMTPServer):
     
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        print ('Receiving message from:', peer)
-        print ('Message addressed from:', mailfrom)
-        print ('Message addressed to  :', rcpttos)
-        print ('Message length        :', len(data))
+        
+        data_as_string = str(data)
+        message_text = data_as_string.split('Subject:')[1]
+
+        if classify(message_text):
+        	print ('Mail is spam! exiting...')
+        	return
+        else:
+        	print ('Mail is okay, keep proccesing...')
+        	print ('Receiving message from:', peer)
+        	print ('Message addressed from:', mailfrom)
+        	print ('Message addressed to  :', rcpttos)
+        	print ('Message length        :', len(data))
+        	print ('Message               :', message_text)
+        	#subject = message_text.split('\n\n')[0]
+        	#body = message_text.split('\n\n')[1]
+        	#print ('Subject               :', subject)
+        	#print ('Body                  :', body)
+
+     
         return
 
 server = CustomSMTPServer(('127.0.0.1', 1025), None)
